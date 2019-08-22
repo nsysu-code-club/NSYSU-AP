c694985882fb43ef9d479813c742fe77c4f2167c
RainVisitor <abc873693@gmail.com>
2019-08-22 14:32:11 +0800

    Update feature admission guide page
    Add:
     back and forward button

--- lib/pages/admission_guide_page.dart
+++ lib/pages/admission_guide_page.dart
@@ -8,8 +8,14 @@ class AdmissionGuidePage extends StatefulWidget {
   _AdmissionGuidePageState createState() => _AdmissionGuidePageState();
 }
 
+enum _State { loading, finish, error, empty, offlineEmpty }
+
 class _AdmissionGuidePageState extends State<AdmissionGuidePage> {
   AppLocalizations app;
+
+  _State state = _State.loading;
+
+  WebViewController webViewController;
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +24,42 @@ class _AdmissionGuidePageState extends State<AdmissionGuidePage> {
       appBar: AppBar(
         title: Text(app.admissionGuide),
         backgroundColor: Resource.Colors.blue,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () async {
+              if ((await webViewController?.canGoBack() ?? false))
+                webViewController?.goBack();
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () async {
+              if ((await webViewController?.canGoForward() ?? false))
+                webViewController?.goForward();
+            },
+          ),
+        ],
       ),
       body: WebView(
         initialUrl: 'https://leslietsai1.wixsite.com/nsysufreshman',
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (WebViewController webViewController) {
-          // _controller.complete(webViewController);
+          this.webViewController = webViewController;
+          //_controller.complete(webViewController);
         },
       ),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+      name: 'Toaster',
+      onMessageReceived: (JavascriptMessage message) {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(content: Text(message.message)),
+        );
+      },
     );
   }
 }
