75c8b520972352ed30a42e08bd12e8c82c4f6f15
RainVisitor <abc873693@gmail.com>
2019-08-23 23:30:52 +0800

    Refactor push page to cupertino style
    Code tune drawer body

--- lib/widgets/drawer_body.dart
+++ lib/widgets/drawer_body.dart
@@ -11,9 +11,6 @@ import 'package:nsysu_ap/res/resource.dart' as Resource;
 import 'package:nsysu_ap/utils/app_localizations.dart';
 import 'package:nsysu_ap/utils/utils.dart';
 import 'package:nsysu_ap/widgets/share_data_widget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-var pictureUrl = "";
 
 class DrawerBody extends StatefulWidget {
   final UserInfo userInfo;
@@ -25,14 +22,13 @@ class DrawerBody extends StatefulWidget {
 }
 
 class DrawerBodyState extends State<DrawerBody> {
-  SharedPreferences prefs;
-  bool displayPicture = true;
-
   AppLocalizations app;
 
   bool isStudyExpanded = false;
-  bool isBusExpanded = false;
-  bool isLeaveExpanded = false;
+  bool displayPicture = true;
+
+  TextStyle get _defaultStyle =>
+      TextStyle(color: Resource.Colors.grey, fontSize: 16.0);
 
   @override
   void initState() {
@@ -43,8 +39,6 @@ class DrawerBodyState extends State<DrawerBody> {
   void dispose() {
     super.dispose();
   }
-
-  _defaultStyle() => TextStyle(color: Resource.Colors.grey, fontSize: 16.0);
 
   @override
   Widget build(BuildContext context) {
@@ -127,32 +121,50 @@ class DrawerBodyState extends State<DrawerBody> {
                     ? Resource.Colors.blue
                     : Resource.Colors.grey,
               ),
-              title: Text(app.courseInfo, style: _defaultStyle()),
+              title: Text(app.courseInfo, style: _defaultStyle),
               children: <Widget>[
-                _subItem(Icons.class_, app.course, CoursePage()),
-                _subItem(Icons.assignment, app.score, ScorePage()),
+                _subItem(
+                  icon: Icons.class_,
+                  title: app.course,
+                  page: CoursePage(),
+                ),
+                _subItem(
+                  icon: Icons.assignment,
+                  title: app.score,
+                  page: ScorePage(),
+                ),
               ],
             ),
             _item(
-              Icons.school,
-              app.graduationCheckChecklist,
-              GraduationReportPage(
+              icon: Icons.school,
+              title: app.graduationCheckChecklist,
+              page: GraduationReportPage(
                 username: ShareDataWidget.of(context).data.username,
                 password: ShareDataWidget.of(context).data.password,
               ),
             ),
             _item(
-              Icons.monetization_on,
-              app.tuitionAndFees,
-              TuitionAndFeesPage(
+              icon: Icons.monetization_on,
+              title: app.tuitionAndFees,
+              page: TuitionAndFeesPage(
                 username: ShareDataWidget.of(context).data.username,
                 password: ShareDataWidget.of(context).data.password,
               ),
             ),
-            _item(Icons.accessibility_new, app.admissionGuide,
-                AdmissionGuidePage()),
-            _item(Icons.face, app.about, AboutUsPage()),
-            _item(Icons.settings, app.settings, SettingPage()),
+            _item(
+                icon: Icons.accessibility_new,
+                title: app.admissionGuide,
+                page: AdmissionGuidePage()),
+            _item(
+              icon: Icons.face,
+              title: app.about,
+              page: AboutUsPage(),
+            ),
+            _item(
+              icon: Icons.settings,
+              title: app.settings,
+              page: SettingPage(),
+            ),
             ListTile(
               leading: Icon(
                 Icons.power_settings_new,
@@ -162,7 +174,10 @@ class DrawerBodyState extends State<DrawerBody> {
                 Navigator.popUntil(
                     context, ModalRoute.withName(Navigator.defaultRouteName));
               },
-              title: Text(app.logout, style: _defaultStyle()),
+              title: Text(
+                app.logout,
+                style: _defaultStyle,
+              ),
             ),
           ],
         ),
@@ -170,19 +185,29 @@ class DrawerBodyState extends State<DrawerBody> {
     );
   }
 
-  _item(IconData icon, String title, Widget page) => ListTile(
+  _item({
+    @required IconData icon,
+    @required String title,
+    @required Widget page,
+  }) =>
+      ListTile(
         leading: Icon(icon, color: Resource.Colors.grey),
-        title: Text(title, style: _defaultStyle()),
+        title: Text(title, style: _defaultStyle),
         onTap: () {
           Navigator.pop(context);
           Utils.pushCupertinoStyle(context, page);
         },
       );
 
-  _subItem(IconData icon, String title, Widget page) => ListTile(
+  _subItem({
+    @required IconData icon,
+    @required String title,
+    @required Widget page,
+  }) =>
+      ListTile(
         contentPadding: EdgeInsets.symmetric(horizontal: 72.0),
         leading: Icon(icon, color: Resource.Colors.grey),
-        title: Text(title, style: _defaultStyle()),
+        title: Text(title, style: _defaultStyle),
         onTap: () async {
           Navigator.of(context).pop();
           Utils.pushCupertinoStyle(context, page);
