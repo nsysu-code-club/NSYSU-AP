41a6fcb49c62fce40237b6eb53e7165588f076f1
RainVisitor <abc873693@gmail.com>
2020-02-14 18:03:06 +0800

    Update firebase crashlytics by official plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,22 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_crashlytics/flutter_crashlytics.dart';
 import 'package:nsysu_ap/config/constants.dart';
 
 import 'app.dart';
 
 void main() async {
-  bool isInDebugMode = Constants.isInDebugMode;
-  if (Platform.isIOS || Platform.isAndroid) {
-    if (!Constants.isInDebugMode) {
-      FlutterError.onError = (FlutterErrorDetails details) {
-        if (isInDebugMode) {
-          // In development mode simply print to console.
-          FlutterError.dumpErrorToConsole(details);
-        } else {
-          // In production mode report to the application zone to report to
-          // Crashlytics.
-          Zone.current.handleUncaughtError(details.exception, details.stack);
-        }
-      };
+  if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+    Crashlytics.instance.enableInDevMode = Constants.isInDebugMode;
 
-      await FlutterCrashlytics().initialize();
-
-      runZoned<Future<Null>>(() async {
-        runApp(
-          MyApp(),
-        );
-      }, onError: (error, stackTrace) async {
-        // Whenever an error occurs, call the `reportCrash` function. This will send
-        // Dart errors to our dev console or Crashlytics depending on the environment.
-        await FlutterCrashlytics()
-            .reportCrash(error, stackTrace, forceCrash: false);
-      });
-    } else {
-      runApp(
-        MyApp(),
-      );
-    }
+    // Pass all uncaught errors from the framework to Crashlytics.
+    FlutterError.onError = Crashlytics.instance.recordFlutterError;
+    runZoned<Future<void>>(() async {
+      runApp(MyApp());
+    }, onError: Crashlytics.instance.recordError);
   } else {
     // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
