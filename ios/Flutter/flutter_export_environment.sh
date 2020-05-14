8e8d559dd309d2d62df921aa736cc5c3f083cb4a
RainVisitor <abc873693@gmail.com>
2020-05-14 21:18:28 +0800

    Update iOS setting for flutter 1.17

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rainvisitor/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rainvisitor/Documents/GitHub/NSYSU-AP"
+export "FLUTTER_APPLICATION_PATH=/Users/rainvisitor/Documents/GitHub/nsysu_ap"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/rainvisitor/development/flutter/bin/cache/artifacts/engine/ios-release"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rainvisitor/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.3.0"
 export "FLUTTER_BUILD_NUMBER=300"
