1c7ece163a1be490ee6f73eec4e157de60aec239
RainVisitor <abc873693@gmail.com>
2020-09-10 23:20:02 +0800

    Fix firebase crashlytics record error

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:ap_common/utils/preferences.dart';
 import 'package:ap_common_firebase/utils/firebase_utils.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nsysu_ap/config/constants.dart';
 
@@ -21,7 +20,11 @@ void main() async {
   await Firebase.initializeApp();
   if (FirebaseUtils.isSupportCrashlytics) {
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  }
-  runApp(MyApp());
+    runZonedGuarded(() {
+      runApp(MyApp());
+    }, (error, stackTrace) {
+      FirebaseCrashlytics.instance.recordError(error, stackTrace);
+    });
+  } else
+    runApp(MyApp());
 }
