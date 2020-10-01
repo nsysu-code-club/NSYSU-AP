a6c2241bc547323fd82917b1aaa2f98962418016
RainVisitor <abc873693@gmail.com>
2020-10-01 17:49:34 +0800

    Add iOS course home widget

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import WidgetKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -14,6 +15,21 @@ import Flutter
     if(!UserDefaults.standard.bool(forKey: "Notification")) {
         UIApplication.shared.cancelAllLocalNotifications()
         UserDefaults.standard.set(true, forKey: "Notification")
+    }
+    //Course app widget must be iOS 14 above
+    if #available(iOS 14.0, *) {
+        //Course data export to app group
+        let standrtUserDefaults = UserDefaults.standard
+        print(UserDefaults.standard.dictionaryRepresentation().keys)
+        let groupUserDefaults = UserDefaults(suiteName: "group.ap_common.course_app_widget")
+        if let semester = standrtUserDefaults.string(forKey: "flutter.ap_common.current_semester_code"){
+            print("sememster \(semester)")
+            if let text = standrtUserDefaults.string(forKey: "flutter.ap_common.course_data_\(semester)"){
+                print("text \(text)")
+                groupUserDefaults?.set(text, forKey: "course_notify")
+            }
+        }
+        WidgetCenter.shared.reloadAllTimelines()
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
