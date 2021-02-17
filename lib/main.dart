fc4936582b40e199035d0061bcf1f75496298b2c
RainVisitor <abc873693@gmail.com>
2021-02-17 23:51:18 +0800

    Improve firebase initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:ap_common/models/course_data.dart';
 import 'package:ap_common/utils/preferences.dart';
 import 'package:ap_common_firebase/utils/firebase_utils.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nsysu_ap/config/constants.dart';
 
 import 'app.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  bool isInDebugMode = Constants.isInDebugMode;
-  HttpClient.enableTimelineLogging = isInDebugMode;
   await Preferences.init(
     key: Constants.key,
     iv: Constants.iv,
   );
-  await Firebase.initializeApp();
-  if (FirebaseUtils.isSupportCrashlytics) {
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  if (FirebaseUtils.isSupportCore) await Firebase.initializeApp();
+  if (!kDebugMode && FirebaseUtils.isSupportCrashlytics) {
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
     runZonedGuarded(() {
       runApp(MyApp());
