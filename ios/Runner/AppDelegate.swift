28d8ac751ee0996a2b7461659f91788cc3876d49
RainVisitor <abc873693@gmail.com>
2021-02-18 01:20:14 +0800

    Remove iOS app delegate print

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -20,12 +20,9 @@ import WidgetKit
     if #available(iOS 14.0, *) {
         //Course data export to app group
         let standrtUserDefaults = UserDefaults.standard
-        print(UserDefaults.standard.dictionaryRepresentation().keys)
         let groupUserDefaults = UserDefaults(suiteName: "group.com.nsysu.ap")
         if let semester = standrtUserDefaults.string(forKey: "flutter.ap_common.current_semester_code"){
-            print("sememster \(semester)")
             if let text = standrtUserDefaults.string(forKey: "flutter.ap_common.course_data_\(semester)"){
-                print("text \(text)")
                 groupUserDefaults?.set(text, forKey: "course_notify")
             }
         }
