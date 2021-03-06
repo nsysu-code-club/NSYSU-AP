25b3b641607827596de84efad806057c21d4b39b
RainVisitor <abc873693@gmail.com>
2021-03-06 19:21:59 +0800

    Update tow car subscription page

---
+++ lib/models/car_park_area.dart
@@ -0,0 +1,13 @@
+class CarParkArea {
+  final String fcmTopic;
+  final String name;
+  final String imageUrl;
+  bool enable;
+
+  CarParkArea({
+    this.fcmTopic,
+    this.name,
+    this.imageUrl,
+    this.enable = false,
+  });
+}
--- lib/pages/tow/tow_car_subscription_page.dart
+++ lib/pages/tow/tow_car_subscription_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nsysu_ap/models/car_park_area.dart';
 
 class TowCarSubscriptionPage extends StatefulWidget {
   @override
@@ -6,8 +7,40 @@ class TowCarSubscriptionPage extends StatefulWidget {
 }
 
 class _TowCarSubscriptionPageState extends State<TowCarSubscriptionPage> {
+  bool enableAll = false;
+
+  List<CarParkArea> carParkAreas = [
+    CarParkArea(name: '武嶺'),
+    CarParkArea(name: 'L停'),
+    CarParkArea(name: '管院停車場'),
+    CarParkArea(name: '武嶺'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: [
+        SwitchListTile.adaptive(
+          title: Text('全部區域'),
+          value: enableAll,
+          onChanged: (value) {
+            setState(() {
+              enableAll = value;
+              carParkAreas.forEach((element) {
+                element.enable = enableAll;
+              });
+            });
+          },
+        ),
+        for (CarParkArea carParkArea in carParkAreas ?? [])
+          SwitchListTile.adaptive(
+            title: Text(carParkArea.name),
+            value: carParkArea.enable,
+            onChanged: (value) {
+              setState(() => carParkArea.enable = value);
+            },
+          ),
+      ],
+    );
   }
 }
