eb3efa63bc8051dfaf36ed9f247672edbf7a3e3e
Rainvisitor <abc873693@gmail.com>
2023-06-10 19:53:33 +0800

    fix: loss iOS build files

--- scripts/zip_dev_configs.sh
+++ scripts/zip_dev_configs.sh
@@ -4,5 +4,7 @@ zip -r dev_configs.zip \
     lib/firebase_options.dart \
     lib/config/sdk_constants.dart \
     android/app/google-services.json \
+    ios/Runner/GoogleService-Info.plist \
     ios/firebase_app_id_file.json \
+    macos/Runner/GoogleService-Info.plist \
     macos/firebase_app_id_file.json
