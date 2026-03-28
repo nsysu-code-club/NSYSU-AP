1e36e36acd76687f76634f7275cd44bd2a746960
Rainvisitor <abc873693@gmail.com>
2026-03-28 22:24:26 +0800

    feat(home): refactor drawer with new DrawerMenuItem widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,7 +39,7 @@ class HomePageState extends State<HomePage> {
   final GlobalKey<HomePageScaffoldState> _homeKey =
       GlobalKey<HomePageScaffoldState>();
 
-  bool get isMobile => MediaQuery.of(context).size.shortestSide < 680;
+  bool get isTablet => MediaQuery.of(context).size.shortestSide > 680;
 
   late AppLocalizations app;
   late ApLocalizations ap;
@@ -57,8 +57,15 @@ class HomePageState extends State<HomePage> {
   bool isStudyExpanded = false;
   bool isSchoolNavigationExpanded = false;
 
-  TextStyle get _defaultStyle =>
-      TextStyle(color: ApTheme.of(context).grey, fontSize: 16.0);
+  String get drawerIcon {
+    switch (ApTheme.of(context).brightness) {
+      case Brightness.light:
+        return ImageAssets.nsysu;
+      case Brightness.dark:
+      default:
+        return ImageAssets.nsysu;
+    }
+  }
 
   @override
   void initState() {
@@ -152,177 +159,10 @@ class HomePageState extends State<HomePage> {
           AnnouncementContentPage(announcement: announcement),
         );
       },
-      drawer: ApDrawer(
-        userInfo: userInfo,
-        widgets: <Widget>[
-          if (!isMobile)
-            DrawerItem(
-              icon: ApIcon.home,
-              title: ap.home,
-              onTap: () {
-                setState(() => content = null);
-              },
-            ),
-          ExpansionTile(
-            initiallyExpanded: isStudyExpanded,
-            onExpansionChanged: (bool bool) {
-              setState(() => isStudyExpanded = bool);
-            },
-            leading: Icon(
-              ApIcon.collectionsBookmark,
-              color: isStudyExpanded
-                  ? ApTheme.of(context).blueAccent
-                  : ApTheme.of(context).grey,
-            ),
-            title: Text(ap.courseInfo, style: _defaultStyle),
-            children: <Widget>[
-              DrawerSubItem(
-                icon: ApIcon.classIcon,
-                title: ap.course,
-                onTap: () => _openPage(CoursePage(), needLogin: true),
-              ),
-              DrawerSubItem(
-                icon: ApIcon.assignment,
-                title: ap.score,
-                onTap: () => _openPage(ScorePage(), needLogin: true),
-              ),
-            ],
-          ),
-          DrawerItem(
-            icon: ApIcon.directionsBus,
-            title: ap.bus,
-            onTap: () =>
-                _openPage(BusListPage(locale: Locale(Intl.defaultLocale!))),
-          ),
-          ExpansionTile(
-            initiallyExpanded: isSchoolNavigationExpanded,
-            onExpansionChanged: (bool bool) {
-              setState(() => isSchoolNavigationExpanded = bool);
-            },
-            leading: Icon(
-              ApIcon.navigation,
-              color: isSchoolNavigationExpanded
-                  ? ApTheme.of(context).blueAccent
-                  : ApTheme.of(context).grey,
-            ),
-            title: Text(ap.schoolNavigation, style: _defaultStyle),
-            children: <Widget>[
-              DrawerSubItem(
-                icon: ApIcon.map,
-                title: ap.schoolMap,
-                onTap: () =>
-                    _openPage(SchoolMapPage(), useCupertinoRoute: false),
-              ),
-              DrawerSubItem(
-                icon: ApIcon.accessibilityNew,
-                title: ap.admissionGuide,
-                onTap: () {
-                  if (kIsWeb ||
-                      Platform.isAndroid ||
-                      Platform.isIOS ||
-                      Platform.isMacOS ||
-                      Platform.isWindows) {
-                    _openPage(AdmissionGuidePage(), useCupertinoRoute: false);
-                  } else {
-                    openDesktopWebViewPage(
-                      Constants.admissionGuideUrl,
-                      title: ap.admissionGuide,
-                    );
-                  }
-                },
-              ),
-            ],
-          ),
-          DrawerItem(
-            icon: ApIcon.school,
-            title: app.graduationCheckChecklist,
-            onTap: () =>
-                _openPage(const GraduationReportPage(), needLogin: true),
-          ),
-          DrawerItem(
-            icon: ApIcon.monetizationOn,
-            title: app.tuitionAndFees,
-            onTap: () => _openPage(const TuitionAndFeesPage(), needLogin: true),
-          ),
-          // DrawerItem(
-          //   icon: Icons.car_repair,
-          //   title: app.towCarHelper,
-          //   onTap: () => _openPage(
-          //     TowCarHomePage(),
-          //     useCupertinoRoute: false,
-          //   ),
-          // ),
-          DrawerItem(
-            icon: ApIcon.info,
-            title: ap.schoolInfo,
-            onTap: () => _openPage(SchoolInfoPage(), useCupertinoRoute: false),
-          ),
-          DrawerItem(
-            icon: ApIcon.face,
-            title: ap.about,
-            onTap: () => _openPage(
-              AboutUsPage(
-                assetImage: ImageAssets.nsysu,
-                githubName: 'nsysu-code-club',
-                email: 'nsysu.gdsc@gmail.com',
-                appLicense: app.aboutOpenSourceContent,
-                fbFanPageId: '100906232372556',
-                instagramUsername: 'gdsc_nsysu',
-                fbFanPageUrl: 'https://www.facebook.com/NSYSUGDSC',
-                githubUrl: 'https://github.com/nsysu-code-club',
-              ),
-            ),
-          ),
-          DrawerItem(
-            icon: ApIcon.settings,
-            title: ap.settings,
-            onTap: () => _openPage(SettingPage()),
-          ),
-          if (isLogin)
-            ListTile(
-              leading: Icon(
-                ApIcon.powerSettingsNew,
-                color: ApTheme.of(context).grey,
-              ),
-              onTap: () async {
-                PreferenceUtil.instance.setBool(Constants.prefAutoLogin, false);
-                await PreferenceUtil.instance.setBool(
-                  Constants.prefAutoLogin,
-                  false,
-                );
-                SelcrsHelper.instance.logout();
-                GraduationHelper.instance.logout();
-                TuitionHelper.instance.logout();
-                setState(() {
-                  ShareDataWidget.of(context)!.data.isLogin = false;
-                  ShareDataWidget.of(context)!.data.userInfo = null;
-                });
-                if (isMobile) {
-                  if (!context.mounted) return;
-                  Navigator.of(context).pop();
-                }
-                _checkLoginState();
-              },
-              title: Text(ap.logout, style: _defaultStyle),
-            ),
-        ],
-        onTapHeader: () {
-          if (isLogin) {
-            if (userInfo != null && isLogin) {
-              ApUtils.pushCupertinoStyle(
-                context,
-                UserInfoPage(userInfo: userInfo!),
-              );
-            }
-          } else {
-            if (isMobile) Navigator.of(context).pop();
-            openLoginPage();
-          }
-        },
-      ),
+      drawer: _buildDrawer(),
       announcements: announcements,
       onTabTapped: onTabTapped,
-      bottomNavigationBarItems: <Widget>[
+      bottomNavigationBarItems: <NavigationDestination>[
         NavigationDestination(icon: Icon(ApIcon.directionsBus), label: ap.bus),
         NavigationDestination(icon: Icon(ApIcon.classIcon), label: ap.course),
         NavigationDestination(icon: Icon(ApIcon.assignment), label: ap.score),
@@ -498,19 +338,193 @@ class HomePageState extends State<HomePage> {
     }
   }
 
+  Widget _buildDrawer() {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return ApDrawer(
+      userInfo: userInfo,
+      displayPicture: PreferenceUtil.instance.getBool(
+        Constants.prefDisplayPicture,
+        true,
+      ),
+      imageAsset: drawerIcon,
+      onTapHeader: () {
+        if (isLogin) {
+          if (userInfo != null) {
+            ApUtils.pushCupertinoStyle(
+              context,
+              UserInfoPage(userInfo: userInfo!),
+            );
+          }
+        } else {
+          if (!isTablet) Navigator.of(context).pop();
+          openLoginPage();
+        }
+      },
+      widgets: <Widget>[
+        if (isTablet)
+          DrawerMenuItem(
+            icon: ApIcon.home,
+            title: ap.home,
+            onTap: () => setState(() => content = null),
+          ),
+        _buildStudySection(),
+        DrawerMenuItem(
+          icon: ApIcon.directionsBus,
+          title: ap.bus,
+          onTap: () =>
+              _openPage(BusListPage(locale: Locale(Intl.defaultLocale!))),
+        ),
+        _buildSchoolNavigationSection(),
+        DrawerMenuItem(
+          icon: ApIcon.school,
+          title: app.graduationCheckChecklist,
+          onTap: () =>
+              _openPage(const GraduationReportPage(), needLogin: true),
+        ),
+        DrawerMenuItem(
+          icon: ApIcon.monetizationOn,
+          title: app.tuitionAndFees,
+          onTap: () =>
+              _openPage(const TuitionAndFeesPage(), needLogin: true),
+        ),
+        DrawerMenuItem(
+          icon: ApIcon.info,
+          title: ap.schoolInfo,
+          onTap: () =>
+              _openPage(SchoolInfoPage(), useCupertinoRoute: false),
+        ),
+        DrawerMenuItem(
+          icon: ApIcon.face,
+          title: ap.about,
+          onTap: () => _openPage(
+            AboutUsPage(
+              assetImage: ImageAssets.nsysu,
+              githubName: 'nsysu-code-club',
+              email: 'nsysu.gdsc@gmail.com',
+              appLicense: app.aboutOpenSourceContent,
+              fbFanPageId: '100906232372556',
+              instagramUsername: 'gdsc_nsysu',
+              fbFanPageUrl: 'https://www.facebook.com/NSYSUGDSC',
+              githubUrl: 'https://github.com/nsysu-code-club',
+            ),
+          ),
+        ),
+        DrawerMenuItem(
+          icon: ApIcon.settings,
+          title: ap.settings,
+          onTap: () => _openPage(SettingPage()),
+        ),
+        if (isLogin) ...<Widget>[
+          const DrawerDivider(),
+          DrawerMenuItem(
+            icon: ApIcon.powerSettingsNew,
+            title: ap.logout,
+            iconColor: colorScheme.error,
+            onTap: () async {
+              await PreferenceUtil.instance.setBool(
+                Constants.prefAutoLogin,
+                false,
+              );
+              SelcrsHelper.instance.logout();
+              GraduationHelper.instance.logout();
+              TuitionHelper.instance.logout();
+              setState(() {
+                ShareDataWidget.of(context)!.data.isLogin = false;
+                ShareDataWidget.of(context)!.data.userInfo = null;
+              });
+              content = null;
+              if (!isTablet) {
+                if (!context.mounted) return;
+                Navigator.of(context).pop();
+              }
+              _checkLoginState();
+            },
+          ),
+        ],
+      ],
+    );
+  }
+
+  Widget _buildStudySection() {
+    return DrawerMenuSection(
+      icon: ApIcon.school,
+      title: ap.courseInfo,
+      initiallyExpanded: isStudyExpanded,
+      onExpansionChanged: (bool value) {
+        setState(() {
+          isStudyExpanded = value;
+        });
+      },
+      children: <DrawerSubMenuItem>[
+        DrawerSubMenuItem(
+          icon: ApIcon.classIcon,
+          title: ap.course,
+          onTap: () => _openPage(CoursePage(), needLogin: true),
+        ),
+        DrawerSubMenuItem(
+          icon: ApIcon.assignment,
+          title: ap.score,
+          onTap: () => _openPage(ScorePage(), needLogin: true),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSchoolNavigationSection() {
+    return DrawerMenuSection(
+      icon: ApIcon.navigation,
+      title: ap.schoolNavigation,
+      initiallyExpanded: isSchoolNavigationExpanded,
+      onExpansionChanged: (bool value) {
+        setState(() {
+          isSchoolNavigationExpanded = value;
+        });
+      },
+      children: <DrawerSubMenuItem>[
+        DrawerSubMenuItem(
+          icon: ApIcon.map,
+          title: ap.schoolMap,
+          onTap: () =>
+              _openPage(SchoolMapPage(), useCupertinoRoute: false),
+        ),
+        DrawerSubMenuItem(
+          icon: ApIcon.accessibilityNew,
+          title: ap.admissionGuide,
+          onTap: () {
+            if (kIsWeb ||
+                Platform.isAndroid ||
+                Platform.isIOS ||
+                Platform.isMacOS ||
+                Platform.isWindows) {
+              _openPage(AdmissionGuidePage(), useCupertinoRoute: false);
+            } else {
+              openDesktopWebViewPage(
+                Constants.admissionGuideUrl,
+                title: ap.admissionGuide,
+              );
+            }
+          },
+        ),
+      ],
+    );
+  }
+
   Future<void> _openPage(
     Widget page, {
     bool needLogin = false,
     bool useCupertinoRoute = true,
   }) async {
-    if (isMobile) Navigator.of(context).pop();
+    if (!isTablet) Navigator.of(context).pop();
     if (needLogin && !isLogin) {
       UiUtil.instance.showToast(
         context,
         ApLocalizations.of(context).notLoginHint,
       );
     } else {
-      if (isMobile) {
+      if (isTablet) {
+        setState(() => content = page);
+      } else {
         if (useCupertinoRoute) {
           ApUtils.pushCupertinoStyle(context, page);
         } else {
@@ -520,8 +534,6 @@ class HomePageState extends State<HomePage> {
           );
         }
         _checkLoginState();
-      } else {
-        setState(() => content = page);
       }
     }
   }
