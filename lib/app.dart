8192bb0ad939807d7d34387ce739ce1c390d6cd8
TWCKaijin <kaijinkaijin2@gmail.com>
2026-07-12 08:08:11 +0800

    feat(ui): adding student leave page to home page drawer

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,22 @@
 import 'package:ap_common/ap_common.dart'
-    hide TranslationProvider, LocaleSettings, AppLocaleUtils, AppLocale;
+    hide AppLocale, AppLocaleUtils, LocaleSettings, TranslationProvider;
 import 'package:ap_common_firebase/ap_common_firebase.dart';
-import 'package:ap_common_flutter_core/ap_common_flutter_core.dart' as ap_l10n
+import 'package:ap_common_flutter_core/ap_common_flutter_core.dart'
+    as ap_l10n
     show TranslationProvider;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:nsysu_crawler/nsysu_crawler.dart';
 import 'package:nsysu_ap/config/constants.dart';
 import 'package:nsysu_ap/pages/graduation_report_page.dart';
 import 'package:nsysu_ap/pages/home_page.dart';
 import 'package:nsysu_ap/pages/setting_page.dart';
+import 'package:nsysu_ap/pages/student_leave_page.dart';
 import 'package:nsysu_ap/pages/study/course_page.dart';
 import 'package:nsysu_ap/pages/study/score_page.dart';
 import 'package:nsysu_ap/utils/app_localizations.dart';
 import 'package:nsysu_ap/widgets/share_data_widget.dart';
+import 'package:nsysu_crawler/nsysu_crawler.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -46,12 +48,19 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
   @override
   void initState() {
     _analytics = FirebaseUtils.init();
-    themeMode = ThemeMode.values[
-        PreferenceUtil.instance.getInt(Constants.prefThemeModeIndex, 0)];
-    currentColorIndex =
-        PreferenceUtil.instance.getInt(ApTheme.PREF_COLOR_INDEX, 0);
-    final int customColorValue =
-        PreferenceUtil.instance.getInt(ApTheme.PREF_CUSTOM_COLOR, 0);
+    themeMode =
+        ThemeMode.values[PreferenceUtil.instance.getInt(
+          Constants.prefThemeModeIndex,
+          0,
+        )];
+    currentColorIndex = PreferenceUtil.instance.getInt(
+      ApTheme.PREF_COLOR_INDEX,
+      0,
+    );
+    final int customColorValue = PreferenceUtil.instance.getInt(
+      ApTheme.PREF_CUSTOM_COLOR,
+      0,
+    );
     if (currentColorIndex == ApTheme.customColorIndex &&
         customColorValue != 0) {
       customColor = Color(customColorValue);
@@ -140,6 +149,8 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
                         GraduationReportPage.routerName:
                             (BuildContext context) =>
                                 const GraduationReportPage(),
+                        StudentLeavePage.routerName: (BuildContext context) =>
+                            const StudentLeavePage(),
                         SettingPage.routerName: (BuildContext context) =>
                             SettingPage(),
                       },
@@ -154,10 +165,10 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
                       ],
                       localizationsDelegates:
                           const <LocalizationsDelegate<dynamic>>[
-                        GlobalMaterialLocalizations.delegate,
-                        GlobalWidgetsLocalizations.delegate,
-                        GlobalCupertinoLocalizations.delegate,
-                      ],
+                            GlobalMaterialLocalizations.delegate,
+                            GlobalWidgetsLocalizations.delegate,
+                            GlobalCupertinoLocalizations.delegate,
+                          ],
                       supportedLocales: AppLocaleUtils.supportedLocales,
                     );
                   },
@@ -200,8 +211,8 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
   }
 
   Future<void> getUserInfo() async {
-    final ApiResult<UserInfo> result =
-        await SelcrsHelper.instance.getUserInfo();
+    final ApiResult<UserInfo> result = await SelcrsHelper.instance
+        .getUserInfo();
     if (!mounted) return;
     switch (result) {
       case ApiSuccess<UserInfo>(:final UserInfo data):
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:ap_common/ap_common.dart';
 import 'package:ap_common_firebase/ap_common_firebase.dart';
 import 'package:ap_common_plugin/ap_common_plugin.dart';
@@ -8,7 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:nsysu_crawler/nsysu_crawler.dart';
 import 'package:nsysu_ap/config/constants.dart';
 import 'package:nsysu_ap/pages/bus/bus_list_page.dart';
 import 'package:nsysu_ap/pages/graduation_report_page.dart';
@@ -16,6 +13,7 @@ import 'package:nsysu_ap/pages/guide/school_map_page.dart';
 import 'package:nsysu_ap/pages/info/shcool_info_page.dart';
 import 'package:nsysu_ap/pages/login/login_page.dart';
 import 'package:nsysu_ap/pages/setting_page.dart';
+import 'package:nsysu_ap/pages/student_leave_page.dart';
 import 'package:nsysu_ap/pages/study/course_page.dart';
 import 'package:nsysu_ap/pages/study/score_page.dart';
 import 'package:nsysu_ap/pages/tuition_and_fees_page.dart';
@@ -24,6 +22,7 @@ import 'package:nsysu_ap/resources/image_assets.dart';
 import 'package:nsysu_ap/utils/app_localizations.dart';
 import 'package:nsysu_ap/utils/utils.dart';
 import 'package:nsysu_ap/widgets/share_data_widget.dart';
+import 'package:nsysu_crawler/nsysu_crawler.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class HomePage extends StatefulWidget {
@@ -59,7 +58,6 @@ class HomePageState extends State<HomePage> {
       case Brightness.light:
         return ImageAssets.nsysu;
       case Brightness.dark:
-      default:
         return ImageAssets.nsysu;
     }
   }
@@ -280,8 +278,7 @@ class HomePageState extends State<HomePage> {
       final dynamic rawContent = map[ap.locale];
       final String? updateNoteContent = switch (rawContent) {
         final String s => s,
-        final List<dynamic> l =>
-          l.map((dynamic e) => '* $e').join('\n'),
+        final List<dynamic> l => l.map((dynamic e) => '* $e').join('\n'),
         _ => null,
       };
       if (!mounted) return;
@@ -345,101 +342,108 @@ class HomePageState extends State<HomePage> {
           Constants.prefDisplayPicture,
           true,
         ),
-      onTapHeader: () {
-        if (isLogin) {
-          if (userInfo != null) {
-            ApUtils.pushCupertinoStyle(
-              context,
-              UserInfoPage(userInfo: userInfo!),
-            );
+        onTapHeader: () {
+          if (isLogin) {
+            if (userInfo != null) {
+              ApUtils.pushCupertinoStyle(
+                context,
+                UserInfoPage(userInfo: userInfo!),
+              );
+            }
+          } else {
+            if (!isTablet) Navigator.of(context).pop();
+            openLoginPage();
           }
-        } else {
-          if (!isTablet) Navigator.of(context).pop();
-          openLoginPage();
-        }
-      },
-      widgets: <Widget>[
-        if (isTablet)
+        },
+        widgets: <Widget>[
+          if (isTablet)
+            DrawerMenuItem(
+              icon: ApIcon.home,
+              title: ap.home,
+              onTap: () => setState(() => content = null),
+            ),
+          _buildStudySection(),
           DrawerMenuItem(
-            icon: ApIcon.home,
-            title: ap.home,
-            onTap: () => setState(() => content = null),
+            icon: ApIcon.directionsBus,
+            title: ap.bus,
+            onTap: () =>
+                _openPage(BusListPage(locale: Locale(Intl.defaultLocale!))),
           ),
-        _buildStudySection(),
-        DrawerMenuItem(
-          icon: ApIcon.directionsBus,
-          title: ap.bus,
-          onTap: () =>
-              _openPage(BusListPage(locale: Locale(Intl.defaultLocale!))),
-        ),
-        _buildSchoolNavigationSection(),
-        DrawerMenuItem(
-          icon: ApIcon.school,
-          title: app.graduationCheckChecklist,
-          onTap: () => _openPage(const GraduationReportPage(), needLogin: true),
-        ),
-        DrawerMenuItem(
-          icon: ApIcon.monetizationOn,
-          title: app.tuitionAndFees,
-          onTap: () => _openPage(const TuitionAndFeesPage(), needLogin: true),
-        ),
-        DrawerMenuItem(
-          icon: ApIcon.info,
-          title: ap.schoolInfo,
-          onTap: () => _openPage(SchoolInfoPage(), useCupertinoRoute: false),
-        ),
-        DrawerMenuItem(
-          icon: ApIcon.face,
-          title: ap.about,
-          onTap: () => _openPage(
-            AboutUsPage(
-              assetImage: ImageAssets.nsysu,
-              githubName: 'nsysu-code-club',
-              email: 'nsysu.gdsc@gmail.com',
-              appLicense: app.aboutOpenSourceContent,
-              fbFanPageId: '100906232372556',
-              instagramUsername: 'gdsc_nsysu',
-              fbFanPageUrl: 'https://www.facebook.com/NSYSUGDSC',
-              githubUrl: 'https://github.com/nsysu-code-club',
+          _buildSchoolNavigationSection(),
+          DrawerMenuItem(
+            icon: ApIcon.school,
+            title: app.graduationCheckChecklist,
+            onTap: () =>
+                _openPage(const GraduationReportPage(), needLogin: true),
+          ),
+          DrawerMenuItem(
+            icon: ApIcon.monetizationOn,
+            title: app.tuitionAndFees,
+            onTap: () => _openPage(const TuitionAndFeesPage(), needLogin: true),
+          ),
+          DrawerMenuItem(
+            icon: Icons.event_busy,
+            title: app.studentLeave,
+            onTap: () => _openPage(const StudentLeavePage(), needLogin: true),
+          ),
+          DrawerMenuItem(
+            icon: ApIcon.info,
+            title: ap.schoolInfo,
+            onTap: () => _openPage(SchoolInfoPage(), useCupertinoRoute: false),
+          ),
+          DrawerMenuItem(
+            icon: ApIcon.face,
+            title: ap.about,
+            onTap: () => _openPage(
+              AboutUsPage(
+                assetImage: ImageAssets.nsysu,
+                githubName: 'nsysu-code-club',
+                email: 'nsysu.gdsc@gmail.com',
+                appLicense: app.aboutOpenSourceContent,
+                fbFanPageId: '100906232372556',
+                instagramUsername: 'gdsc_nsysu',
+                fbFanPageUrl: 'https://www.facebook.com/NSYSUGDSC',
+                githubUrl: 'https://github.com/nsysu-code-club',
+              ),
             ),
           ),
-        ),
-        DrawerMenuItem(
-          icon: ApIcon.settings,
-          title: ap.settings,
-          onTap: () => _openPage(SettingPage()),
-        ),
-        if (isLogin) ...<Widget>[
-          const DrawerDivider(),
           DrawerMenuItem(
-            icon: ApIcon.powerSettingsNew,
-            title: ap.logout,
-            iconColor: colorScheme.error,
-            onTap: () async {
-              await PreferenceUtil.instance.setBool(
-                Constants.prefAutoLogin,
-                false,
-              );
-              SelcrsHelper.instance.logout();
-              GraduationHelper.instance.logout();
-              TuitionHelper.instance.logout();
-              await ApCommonPlugin.clearCourseWidget();
-              setState(() {
-                ShareDataWidget.of(context)!.data.isLogin = false;
-                ShareDataWidget.of(context)!.data.userInfo = null;
-                courseData = null;
-              });
-              content = null;
-              if (!isTablet) {
-                if (!context.mounted) return;
-                Navigator.of(context).pop();
-              }
-              _checkLoginState();
-            },
+            icon: ApIcon.settings,
+            title: ap.settings,
+            onTap: () => _openPage(SettingPage()),
           ),
+          if (isLogin) ...<Widget>[
+            const DrawerDivider(),
+            DrawerMenuItem(
+              icon: ApIcon.powerSettingsNew,
+              title: ap.logout,
+              iconColor: colorScheme.error,
+              onTap: () async {
+                await PreferenceUtil.instance.setBool(
+                  Constants.prefAutoLogin,
+                  false,
+                );
+                SelcrsHelper.instance.logout();
+                GraduationHelper.instance.logout();
+                TuitionHelper.instance.logout();
+                StudentLeaveHelper.instance.logout();
+                await ApCommonPlugin.clearCourseWidget();
+                setState(() {
+                  ShareDataWidget.of(context)!.data.isLogin = false;
+                  ShareDataWidget.of(context)!.data.userInfo = null;
+                  courseData = null;
+                });
+                content = null;
+                if (!isTablet) {
+                  if (!context.mounted) return;
+                  Navigator.of(context).pop();
+                }
+                _checkLoginState();
+              },
+            ),
+          ],
         ],
-      ],
-    ),
+      ),
     );
   }
 
@@ -476,8 +480,8 @@ class HomePageState extends State<HomePage> {
                   title: Text(app.openingBrowserTitle),
                   content: Text(
                     '${app.openingBrowserContent}:\n'
-                    '${Constants.courseSelectorUrl}'
-                    ),
+                    '${Constants.courseSelectorUrl}',
+                  ),
                   actions: <Widget>[
                     TextButton(
                       onPressed: () => Navigator.of(dialogContext).pop(false),
@@ -494,7 +498,9 @@ class HomePageState extends State<HomePage> {
               PlatformUtil.instance.launchUrl(uri.toString());
             } else {
               UiUtil.instance.showToast(context, app.visitingUnSafeLink);
-              debugPrint('Attempted to launch an insecure URL: ${Constants.courseSelectorUrl}');
+              debugPrint(
+                'Attempted to launch an insecure URL: ${Constants.courseSelectorUrl}',
+              );
             }
           },
         ),
